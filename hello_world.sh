954bd22e2ce9969410cfedc4cf802f6dc53529be
gregorthebigmac@gmail.com <gregorthebigmac@gmail.com>
2019-01-16 09:29:49 -0600

    initial commit

--- hello_world.sh
+++ hello_world.sh
@@ -1,1 +1,8 @@
-echo 'hello world'
+#!/bin/sh
+
+cat /etc/hostname	# host name
+uptime				# how long has the system been up
+last reboot			# when was the system rebooted
+uname -a			# linux system information
+free -h				# how much free memory is there in the system
+ps -A				# running processes
