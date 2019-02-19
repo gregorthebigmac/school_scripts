b0356015d894932c0eb2c05e1072ab649eb61867
gregorthebigmac <gregorthebigmac@gmail.com>
2019-02-19 09:26:37 -0600

    GOt the loops working. Now I just need to finish t :/

--- homework2.sh
+++ homework2.sh
@@ -5,12 +5,15 @@ sz_fname=${#1}
 sz_dintl=${#2}
 sz_lname=${#3}
 
-counter=0
-while [ $counter -lt $sz_fname ]
+counter=$(( sz_fname+sz_dintl+sz_lname ))
+echo "counter = [$counter]"
+counter-=$sz_fname
+echo "counter = [$counter]"
+while [ $counter -gt 0 ]
 do
   for name in $names
     do
       echo $name
   done
-  (( counter++ ))
+  (( counter-- ))
 done
