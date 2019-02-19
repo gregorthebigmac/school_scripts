a59c33b0d618d82bc36df5c536d58011d22afe07
gray fox <gfox@fs-main.outerheaven>
2019-02-19 07:34:52 -0600

    I think I finally got it working.

--- homework2.sh
+++ homework2.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 names="$1 $2 $3"
-sz_fname=#{$1}
-sz_dintl=#{$2}
-sz_lname=#{$3}
+sz_fname=${#1}
+sz_dintl=${#2}
+sz_lname=${#3}
 
-for name in $names
+counter=0
+while [ $counter -lt $sz_fname ]
 do
-echo $name
+  for name in $names
+    do
+      echo $name
+  done
+  (( counter++ ))
 done
