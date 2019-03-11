ffb48635edfddadbf46c3b6aad5840dea63acabd
gregorthebigmac <gregorthebigmac@gmail.com>
2019-03-11 16:17:26 -0500

    got an interesting result. Script quits after successful sudo -i.

--- if_command.sh
+++ if_command.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 if ls -la --color
-    then
-    if cd /root
+    then if cd /root
         then echo "this shouldn't have worked."
-    else echo "this failed, and it should have."
+    else if sudo -i
+        then if cd /root
+            then if ls -la --color
+                then echo "wow, this actually worked!"
+            else echo "failed to ls"
+            fi
+        else echo "cd /root still failed"
+        fi
+    else echo "failed to get root."
+    fi
     fi
 else echo "something went wrong."
 fi
