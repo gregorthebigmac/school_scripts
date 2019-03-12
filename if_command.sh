256b848e806c647e4c5d554b59fa31b69decda77
gregorthebigmac <gregorthebigmac@gmail.com>
2019-03-12 10:56:10 -0500

    testing complex if-else logic

--- if_command.sh
+++ if_command.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if ls -la --color
-    then if cd /root
-        then echo "this shouldn't have worked."
-    else if sudo -i
-        then if cd /root
-            then if ls -la --color
-                then echo "wow, this actually worked!"
-            else echo "failed to ls"
-            fi
-        else echo "cd /root still failed"
-        fi
-    else echo "failed to get root."
-    fi
-    fi
-else echo "something went wrong."
+echo "Checking for any ROS repositories already installed..."
+if [ ! -f /etc/apt/sources.list.d/ros-latest.list ]; then
+  echo "Setting up the official ROS repository..."
+  echo "Adding ROS GPG key to apt..."
+  if sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116; then
+    echo "[ok]"
+  else echo "[fail]"
+  exit 23
+  fi
+  echo "Downloading and installing ROS $DISTRO ..."
+  if sudo bash -c "echo \"deb http://packages.ros.org/ros/ubuntu $DISTRO main\" > /etc/apt/sources.list.d/ros-latest.list"; then
+    echo "[ok]"
+  else echo "[fail]"
+  exit 24
+  fi
+  else echo "[found]
+exiting..."
 fi
